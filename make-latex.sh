ee338a0bbcb4d450ea9c3ff1cfd2d1f87aae503e
Σάμιος Γεώργιος <92584026+p20sami@users.noreply.github.com>
2023-03-12 16:20:30 +0200

    Update make-latex.sh

--- make-latex.sh
+++ make-latex.sh
@@ -1,21 +1,30 @@
 #!/bin/sh
 #assemble and preprocess all the sources files
+echo " Starting convertion of chapters to .tex format "
 
 pandoc text/pre.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/pre.tex
+
 pandoc text/intro.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/intro.tex
 
 for filename in text/ch*.txt; do
    [ -e "$filename" ] || continue
-   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --lua-filter=footnote.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --wrap=none --to latex > latex/"$(basename "$filename" .txt).tex"
+   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=additionfilter.lua --to markdown | pandoc --lua-filter=note.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --lua-filter=footnote.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --wrap=none --to latex > latex/"$(basename "$filename" .txt).tex"
 done
+
+echo " Done converting chapters "
 
 pandoc text/web.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/web.tex
 pandoc text/bio.txt --top-level-division=chapter --to latex > latex/bio.tex
 
 for filename in text/apx*.txt; do
    [ -e "$filename" ] || continue
-   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --to latex > latex/"$(basename "$filename" .txt).tex"
+   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=additionfilter.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --to latex > latex/"$(basename "$filename" .txt).tex"
 done
 
+echo " Converting to pdf... "
 
-sed -i '' 's+Figure+Εικόνα+g' ./latex/ch0*
+pandoc -s latex/*.tex -o book.tex
+
+pandoc -N --quiet --variable "geometry=margin=1.2in" --variable mainfont="Open Sans Italic" --variable sansfont="Open Sans Italic" --variable monofont="Open Sans Italic" --variable fontsize=12pt --variable version=2.0 book.tex  --pdf-engine=xelatex --toc -o book.pdf
+
+echo " Done converting to pdf. "
