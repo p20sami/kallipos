94196feb55592bb1fd0c3354beef28f2031d60df
Σάμιος Γεώργιος <92584026+p20sami@users.noreply.github.com>
2023-03-12 15:38:37 +0200

    Create notefilter.lua

---
+++ notefilter.lua
@@ -0,0 +1,12 @@
+function Image(img)
+  if img.classes:find('additionc4',1) then
+    local f = io.open("additionc4/" .. img.src, 'r')
+    local doc = pandoc.read(f:read('*a'))
+    f:close()
+    local caption = pandoc.utils.stringify(doc.meta.caption) or "No Caption Found"
+    local student = pandoc.utils.stringify(doc.meta.student) or "No Student Found"
+    local id = pandoc.utils.stringify(doc.meta.id) or "No ID Found"
+    local comment = "> " .. caption .. " Ον/μο:" .. student .. " Aριθμός Μητρώου " .. AM
+    return pandoc.RawInline('markdown',comment)
+  end
+end
