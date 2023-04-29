6a50082295dc26835c3a1a57420e57a4e22527ff
Σάμιος Γεώργιος <92584026+p20sami@users.noreply.github.com>
2023-04-29 19:21:36 +0300

    Update additionfilter.lua

--- additionfilter.lua
+++ additionfilter.lua
@@ -1,12 +1,12 @@
 function Image(img)
-  if img.classes:find('additionc4',1) then
-    local f = io.open("additionc4/" .. img.src, 'r')
+  if img.classes:find('additionfilter'',1) then
+    local f = io.open("TheParadoxNotes/" .. img.src, 'r')
     local doc = pandoc.read(f:read('*a'))
     f:close()
     local caption = pandoc.utils.stringify(doc.meta.caption) or "No Caption Found"
     local student = pandoc.utils.stringify(doc.meta.student) or "No Student Found"
-    local id = pandoc.utils.stringify(doc.meta.id) or "No AM Found"
-    local comment = "> " .. caption .. " Ον/μο:" .. student .. " Aριθμός Μητρώου " .. AM
-    return pandoc.RawInline('markdown',comment)
+    local AM = pandoc.utils.stringify(doc.meta.id) or "No AM Found"
+    local add = "> " .. caption .. "\n\n>"  .."**Ον/μο:**" .. student .. " " ..  "**Aριθμός Μητρώου:**" .. AM
+    return pandoc.RawInline('markdown',add)
   end
 end
